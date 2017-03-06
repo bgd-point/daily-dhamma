cfc9320add035d432fd71497e66b4550b5a87a56
Martien Dermawan Tanama <martiendt@gmail.com>
2017-03-06 17:22:14 +0700

    clean up code

--- app/src/main/java/red/point/dailydhamma/MainActivity.java
+++ app/src/main/java/red/point/dailydhamma/MainActivity.java
@@ -34,8 +34,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.activity_main_drawer, menu);
         return true;
     }
 
